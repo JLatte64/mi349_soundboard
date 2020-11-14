3d164f914652fbee28d516232ab5e2b898423cf1
Latta, Jordan Robert <lattajor@msu.edu>
2020-11-14 12:55:36 -0500

    Completed soundboard assignment.

---
+++ soundboard.js
@@ -0,0 +1,71 @@
+var applauseButton = document.getElementById("applause-button")
+
+applauseButton.addEventListener('click', function () {
+    document.getElementById("applause-audio").play();
+})
+applauseButton.addEventListener('mouseenter', function () {
+    document.getElementById("applause-audio").play();
+})
+
+var blipButton = document.getElementById("blip-button")
+
+blipButton.addEventListener('click', function () {
+    document.getElementById("blip-audio").play();
+})
+blipButton.addEventListener('mouseenter', function () {
+    document.getElementById("blip-audio").play();
+})
+
+var boingButton = document.getElementById("boing-button")
+
+boingButton.addEventListener('click', function () {
+    document.getElementById("boing-audio").play();
+})
+boingButton.addEventListener('mouseenter', function () {
+    document.getElementById("boing-audio").play();
+})
+
+var booButton = document.getElementById("boo-button")
+
+booButton.addEventListener('click', function () {
+    document.getElementById("boo-audio").play();
+})
+booButton.addEventListener('mouseenter', function () {
+    document.getElementById("boo-audio").play();
+})
+
+var magicButton = document.getElementById("magic-button")
+
+magicButton.addEventListener('click', function () {
+    document.getElementById("magic-audio").play();
+})
+magicButton.addEventListener('mouseenter', function () {
+    document.getElementById("magic-audio").play();
+})
+
+var floopButton = document.getElementById("floop-button")
+
+floopButton.addEventListener('click', function () {
+    document.getElementById("floop-audio").play();
+})
+floopButton.addEventListener('mouseenter', function () {
+    document.getElementById("floop-audio").play();
+})
+
+var pingButton = document.getElementById("ping-button")
+
+pingButton.addEventListener('click', function () {
+    document.getElementById("ping-audio").play();
+})
+pingButton.addEventListener('mouseenter', function () {
+    document.getElementById("ping-audio").play();
+})
+
+var hahaButton = document.getElementById("laugh-button")
+
+hahaButton.addEventListener('click', function () {
+    document.getElementById("laugh-audio").play();
+})
+hahaButton.addEventListener('mouseenter', function () {
+    document.getElementById("laugh-audio").play();
+})
